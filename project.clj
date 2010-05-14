ceb17a1a084f090fa592f868173b0fafa841539a
Robert Marianski <rob@marianski.com>
2010-05-14 14:55:17 -0400

    add rendering capability

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
                  [org.geotools/gt-main "2.7-SNAPSHOT"]
                  [org.geotools/gt-swing "2.7-SNAPSHOT"]
                  [org.geotools/gt-epsg-hsql "2.7-SNAPSHOT"]
-                 [org.geotools/gt-referencing "2.7-SNAPSHOT"]                 
-                 [org.geotools/gt-postgis "2.7-SNAPSHOT"]                 
+                 [org.geotools/gt-referencing "2.7-SNAPSHOT"]
+                 [org.geotools/gt-postgis "2.7-SNAPSHOT"]
+                 [org.geotools/gt-render "2.7-SNAPSHOT"]
                  [org.geotools/gt-shapefile "2.7-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[swank-clojure "1.1.0"]]
   :main geoscript.core
   :namespace [[geoscript.core]])
-
