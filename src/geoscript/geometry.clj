772dc7622e00a9f2e0b8fad3cc99fff45d484d27
Robert Marianski <rob@marianski.com>
2010-05-15 09:28:12 -0400

    have geometry functions return the jts geometry itself
    previously a mapping had been returned that specified a string type, but the
    type can be inferred from the type of the jts geometry

--- src/geoscript/geometry.clj
+++ src/geoscript/geometry.clj
@@ -26,42 +26,38 @@
 (defn from-wkt
   "Creates a geometry from well known text" 
   [string]
-  (def point (. *reader* read (str string))) 
-  {:type (.getGeometryType point) :geometry point})
+  (.read *reader* (str string)))
 
 (defn create-coord
   "Creates a JTS Coordinate Seq"
-  ([coord] (Coordinate. (first coord) (second coord)))
-  ([x y](Coordinate. x y)))
+  ([coords] (apply create-coord coords))
+  ([x y] (Coordinate. x y))
+  ([x y z] (Coordinate. x y z)))
 
 (defn create-point
   "Creates a JTS Point from a X Y"
   [x y]
-  { :type "Point" :geometry (. *factory* createPoint (create-coord x y))})
+  (.createPoint *factory* (create-coord x y)))
 
 (defn create-line-string
   "Creates a JTS Linear ring"
   [line]
-  {:type "LineString" :geometry
-   (. *factory* createLineString (into-array (map create-coord line)))})
+  (.createLineString *factory* (into-array (map create-coord line))))
 
 (defn create-linear-ring
   "Creates a JTS Linear ring"
   [ring]
-  {:type "LinearRing" :geometry
-  (. *factory* createLinearRing (into-array (map create-coord ring)))})
+  (.createLinearRing *factory* (into-array (map create-coord ring))))
 
 (defn create-polygon
   "Creates JTS Polygon"
-  ([shell]
-  {:type "Polygon" :geometry
-  (. *factory* createPolygon ((create-linear-ring shell) :geometry) nil)})
   ([shell & holes]
-     {:type "Polygon" :geometry
-      (. *factory* createPolygon ((create-linear-ring shell) :geometry)
-         (into-array (map (create-linear-ring :geometry)  holes)))}))
+     (.createPolygon
+      *factory*
+      (create-linear-ring shell)
+      (and holes (into-array (map create-linear-ring holes))))))
 
-(defn create-multi-point  
+(defn create-multi-point
   [points])
 
 (defn create-multi-line-string [])
