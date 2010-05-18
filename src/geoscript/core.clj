34441bad6f57da7ebeb35202b36cefdfdd8d47dc
Ivan Willig <iwillig@gmail.com>
2010-05-17 23:47:31 -0400

    remove old namespace

--- src/geoscript/core.clj
+++
@@ -1,7 +0,0 @@
-(ns geoscript.core
-  (:gen-class)
-  (:use [geoscript.io :only(read-shapefile)]))
-
-(defn -main [ & agrs]
-  (println (map #(.isValid (% :geometry)) ((read-shapefile "/Users/ivanwillig/Data/CENSUS/nycb2000_09c_av/nycb2000.shp") :features))))
-
--- src/geoscript/osm.clj
+++
@@ -1,8 +0,0 @@
-(ns geoscript.openstreetmap
-  (:use [clojure.xml :only(parse)]))
-
-
-(defn get-lastest-planet
-  "downloads the lastest osm planet file"
-  [source dest]
-  )
