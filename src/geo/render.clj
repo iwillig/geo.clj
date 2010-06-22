3e0931dfed725df2b0b46ab9a2022dd879020518
Robert Marianski <rob@marianski.com>
2010-06-22 12:09:40 -0400

    update swing rendering to take an optional title and style object

--- src/geo/render.clj
+++ src/geo/render.clj
@@ -1,6 +1,6 @@
 (ns geo.render
   (:import [org.geotools.data FeatureSource]
-           [javax.imageio ImageIO]          
+           [javax.imageio ImageIO]
            [org.geotools.map DefaultMapContext MapContext]
            [org.geotools.styling SLDParser]
            [org.geotools.factory CommonFactoryFinder]
@@ -11,27 +11,32 @@
 
 (defn make-style
   [path]
-  (first (.readXML (SLDParser. *style-factory* (-> path java.io.File. .toURL)))))
+  (first
+   (.readXML (SLDParser. *style-factory* (-> path java.io.File. .toURL)))))
 
-(defn make-map
-  "builds a DefaultMapContent"
-  [feature-collection sld]
-  (doto (DefaultMapContext.)
-    (.setTitle "Default Map")
-    (.addLayer feature-collection (make-style sld))))
+(defn make-mapcontext
+  "builds a DefaultMapContext
+    Options can be:
+      :title \"Title of JFrame\"
+      :style geotools-style-object"
+  [feature-collection & mapoptions]
+  (let [options (apply hash-map mapoptions)]
+    (doto (DefaultMapContext.)
+      (.setTitle (or (:title options) "Default Map"))
+      (.addLayer feature-collection (:style options)))))
 
-(defn swing [feature-collection]
-  (doto (JMapFrame.
-   (doto (DefaultMapContext.)
-     (.setTitle "Swing!!!!!!!!!!!")
-     (.addLayer feature-collection nil)))
-    (.setDefaultCloseOperation (JFrame/DISPOSE_ON_CLOSE))
-    (.setSize 800 600)
-    (.enableStatusBar true)
-    (.enableToolBar true)
-    (.setVisible true)))
+(defn swing
+  "create a swing frame displaying the features in the geotools
+   featurecollection. See make-mapcontext for options"
+  [feature-collection & frameoptions]
+  (doto (JMapFrame. (apply make-mapcontext feature-collection frameoptions))
+      (.setDefaultCloseOperation (JFrame/DISPOSE_ON_CLOSE))
+      (.setSize 800 600)
+      (.enableStatusBar true)
+      (.enableToolBar true)
+      (.setVisible true)))
 
 (defn render-image
   "renders a images"
   [feature-collection sld]
-  (make-map feature-collection sld))
+  (make-mapcontext feature-collection sld))
