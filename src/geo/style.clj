73c45976a47ed4ee5b85148ec06ec80e11c395fc
Ivan Willig <iwillig@gmail.com>
2011-03-18 09:37:26 -0400

    More work on a styling api for geo.clj.

--- src/geo/style.clj
+++ src/geo/style.clj
@@ -1,8 +1,9 @@
 (ns geo.style
   (:import
    [java.awt Color]
-   [org.geotools.styling SLD]
-   [org.geotools.styling SLDParser StyleBuilder Rule]
+   [org.geotools.styling SLDParser
+    GraphicImpl
+    StyleBuilder Rule SLD RuleImpl]
    [org.geotools.factory CommonFactoryFinder]))
 
 (def *style-factory*       (CommonFactoryFinder/getStyleFactory nil))
@@ -20,12 +21,14 @@
 (defn make-literal [literal]
   (.literal *filter-facotry* literal))
 
-(defn make-fill [{:keys [color opacity]}]
+(defn make-fill [{:keys [color opacity]
+                  :or {color "#ffffff" opacity 1.0}}]
   (.createFill *style-factory*
                  (make-literal (hex->color color))
                  (make-literal opacity)))
 
-(defn make-stroke [{:keys [color width opacity]}]
+(defn make-stroke [{:keys [color width opacity]
+                    :or {color "#808080" width 1.0 opacity 1.0}}]
   (.createStroke *style-factory*
                  (make-literal (hex->color color))
                  (make-literal width)
@@ -33,38 +36,65 @@
 
 ;; simple styling with the SLD interface.
 
-(defn make-point [{:keys [well-know-name line-color fill-color opacity size label font]
-                  :or {well-know-name "Square"
-                       line-color "#808080"
-                       fill-color "#ffffff"
-                       opacity 1.0
-                       size 1.0 }}]
-  (SLD/createPointStyle well-know-name (hex->color line-color) (hex->color fill-color)
+(defn style-point [{:keys [well-know-name
+                          line-color fill-color
+                           opacity size label font]
+                    :or {well-know-name "Square"
+                         line-color "#808080"
+                         fill-color "#ffffff"
+                         opacity 1.0
+                         size 1.0 }}]
+  (SLD/createPointStyle well-know-name (hex->color line-color)
+                        (hex->color fill-color)
                         opacity
                         size
                         label
                         font))
 
-(defn make-line [{:keys [line-color width label font]
+(defn style-line [{:keys [line-color width label font]
                   :or { line-color "#808080" width 1.0}}]
   (SLD/createLineStyle (hex->color line-color) width label font))
 
-(defn make-polygon [{:keys [storke fill opacity label font]
-                :or   {storke "#808080" fill "#ffffff" opacity 1.0}}]
-  (SLD/createPolygonStyle (hex->color storke) (hex->color fill) opacity label font))
+(defn style-polygon [{:keys [storke fill opacity label font]
+                     :or   {storke "#808080" fill "#ffffff" opacity 1.0}}]
+  (SLD/createPolygonStyle (hex->color storke) (hex->color fill)
+                          opacity label font))
 
 
+(defn symbolizer-point [& options]
+  (.createPointSymbolizer *style-builder*))
 
+(defn symbolizer-line [& options]
+  (.createLineSymbolizer *style-builder*))
 
-;;; more complex styling
-;;; not finished 
-(defn make-rule [symbolizers]
-  (let [rule (.createRule *style-builder* (into-array symbolizers))]
-    rule))
+(defn symbolizer-polygon [style]
+  (let [fill (make-fill (:fill style))
+        stroke (make-stroke (:stroke style))]
+    (.createPolygonSymbolizer *style-factory* stroke fill nil)))
+
+(defn make-legend []
+  (GraphicImpl. *filter-facotry*))
+
+(defn make-rule [symbolizers &
+                 {:keys [legend name filter else-filter max min]
+                  :or   {description {:title "Title"
+                                 :description "Description"}
+                    name "Default Style"}}]
+  (doto (.createRule *style-builder* (into-array symbolizers))
+    (.setFilter filter)
+    (.setName name)))
 
 (defn make-style [rules]
   (let [feature-type-style (.createFeatureTypeStyle *style-factory*
                                                     (into-array Rule rules))
         style (.createStyle *style-factory*)]
-    (.add (.featureTypeStyles style)feature-type-style) style))
+    (.add (.featureTypeStyles style) feature-type-style) style))
 
+
+(def style (make-style [(make-rule [(symbolizer-polygon {})]
+                                   :legend (make-legend))
+                        (make-rule [(symbolizer-polygon
+                                     {:fill {:color "#996799"}
+                                      :stroke {:color "#000000"}})]
+                                   :filter
+                                   (make-filter "BoroName = 'Brooklyn'"))]))
